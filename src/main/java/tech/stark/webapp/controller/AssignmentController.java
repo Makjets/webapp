0e273e2d81fc1a2447f955ff97b9c97432a601b6
Makarand <madhavi.m@northeastern.edu>
2023-10-04 13:06:42 -0400

    add points check

--- src/main/java/tech/stark/webapp/controller/AssignmentController.java
+++ src/main/java/tech/stark/webapp/controller/AssignmentController.java
@@ -1,30 +1,31 @@
 package tech.stark.webapp.controller;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import tech.stark.webapp.models.Assignment;
-import tech.stark.webapp.repository.AssignmentRepository;
 import tech.stark.webapp.service.AssignmentService;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/v1/assignments")
 public class AssignmentController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AssignmentController.class);
 
+    @Value("${application.config.min-points}")
+    private int min;
+
+    @Value("${application.config.max-points}")
+    private int max;
     @Autowired
     private AssignmentService assignmentService;
     @GetMapping
@@ -54,7 +55,11 @@ public class AssignmentController {
     @PostMapping
     public ResponseEntity<Assignment> createAssignment(@RequestBody Assignment assignment,
                                                        HttpServletRequest request) {
+        LOGGER.info("min "+min+" max "+max);
+        LOGGER.info(String.valueOf(assignment.getNum_of_attempts()));
         if(request.getQueryString()!=null){
+            return ResponseEntity.badRequest().build();
+        } else if (assignment.getPoints()<min || assignment.getPoints()>max){
             return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(assignmentService.save(assignment));
@@ -79,6 +84,8 @@ public class AssignmentController {
                                                        @RequestBody Assignment assignment,
                                                        HttpServletRequest request) {
         if(request.getQueryString()!=null){
+            return ResponseEntity.badRequest().build();
+        } else if (assignment.getPoints()<min || assignment.getPoints()>max){
             return ResponseEntity.badRequest().build();
         }
         Optional<Assignment> isAssignment = assignmentService.updateAssignment(id,assignment);
