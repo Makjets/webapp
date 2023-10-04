62e959d733a4e398d1f4aa9fbd6b79ac6c798e50
Makarand <madhavi.m@northeastern.edu>
2023-10-03 22:58:02 -0400

    fix health check api

--- src/main/java/tech/stark/webapp/controller/HealthController.java
+++ src/main/java/tech/stark/webapp/controller/HealthController.java
@@ -1,17 +1,20 @@
 package tech.stark.webapp.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.websocket.OnError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestController
 @RequestMapping("/healthz")
@@ -19,10 +22,20 @@ public class HealthController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HealthController.class);
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> greeting(HttpServletRequest request, @RequestBody @Nullable String body) {
         if(request.getQueryString() != null || body != null){
             return ResponseEntity.badRequest().cacheControl(CacheControl.noCache()).build();
+        }
+        try{
+            jdbcTemplate.execute("SELECT 1");
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .cacheControl(CacheControl.noCache())
+                    .build();
         }
 
         return ResponseEntity.ok().cacheControl(CacheControl.noCache()).build();
