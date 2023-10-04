98a47d3ae2e313c98c948c18da05237068dc3040
Makarand <madhavi.m@northeastern.edu>
2023-10-03 22:24:49 -0400

    add assignment crud operations

--- src/main/java/tech/stark/webapp/models/Assignment.java
+++ src/main/java/tech/stark/webapp/models/Assignment.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "Assignment")
 public class Assignment {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.UUID)
     private String id;
     private String name;
     private int points;
---
+++ src/main/java/tech/stark/webapp/service/AssignmentService.java
@@ -0,0 +1,55 @@
+package tech.stark.webapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import tech.stark.webapp.models.Assignment;
+import tech.stark.webapp.repository.AssignmentRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Component
+public class AssignmentService {
+
+    @Autowired
+    private AssignmentRepository assignmentRepository;
+
+    public  List<Assignment> getAssignments(String id) {
+        if(id == null){
+            return StreamSupport.stream(
+                    assignmentRepository.findAll().spliterator(),
+                    false).collect(Collectors.toList());
+        } else {
+            Optional<Assignment> isAssignment = assignmentRepository.findById(id);
+            return isAssignment.map(List::of).orElse(null);
+        }
+
+    }
+
+    public Assignment save(Assignment assignment) {
+        return assignmentRepository.save(assignment);
+    }
+
+    public boolean deleteAssigment(String id) {
+        if(assignmentRepository.existsById(id)){
+            assignmentRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Optional<Assignment> updateAssignment(String id, Assignment assignment) {
+        if(assignmentRepository.existsById(id)){
+            assignment.setId(id);
+            assignmentRepository.save(assignment);
+            return Optional.of(assignment);
+        } else {
+            return Optional.empty();
+        }
+    }
+}
