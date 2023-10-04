29e4ef71ea7fc7f57966ab5c0244a34ee30cd16b
Makarand <madhavi.m@northeastern.edu>
2023-10-03 21:15:23 -0400

    add-db

--- src/main/java/tech/stark/domain/Account.java
+++ src/main/java/tech/stark/domain/Account.java
@@ -6,13 +6,24 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import static javax.persistence.GenerationType.AUTO;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Serdeable
+@Entity
+@Table(name = "Account")
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = AUTO)
     private String id;
     private String first_name;
     private String last_name;
