a4f99efecbb81bb15173c44c332f3a7ad93f7a96
Makarand <madhavi.m@northeastern.edu>
2023-10-04 15:48:59 -0400

    update base test

--- src/test/java/tech/stark/webapp/WebappApplicationTests.java
+++ src/test/java/tech/stark/webapp/WebappApplicationTests.java
@@ -2,12 +2,16 @@ package tech.stark.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class WebappApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
