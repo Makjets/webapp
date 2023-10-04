3d4f671b91b75645c79180e3041ce2c3b0c0a85f
Makarand <madhavi.m@northeastern.edu>
2023-10-04 13:06:42 -0400

    add points check

--- src/main/java/tech/stark/webapp/service/AssignmentService.java
+++ src/main/java/tech/stark/webapp/service/AssignmentService.java
@@ -1,6 +1,7 @@
 package tech.stark.webapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
